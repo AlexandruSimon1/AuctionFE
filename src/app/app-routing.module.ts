7b86da1632df42dfd61f5313ceab000c467233c6
Arthur_Simon <asimon@ms3-inc.com>
2021-02-16 20:19:32 +0200

    Solved issues with main page and now everything is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { AuctionDetailComponent } from './modules/auction/auction-detail/auction-detail.component';
+import { AuctionThumbailComponent } from './modules/auction/auction-thubmnail/auction-thubmnail.component';
 import { AuctionListComponent } from './modules/auction/auction-list/auction-list.component';
 import { ProfileComponent } from './modules/user/profile/profile.component';
 import { HomeComponent } from './modules/home/home.component';
@@ -5,14 +7,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/login/login.component';
 import { RegisterComponent } from './modules/register/register.component';
+import { AuthenticationGuard } from './authentication/authentication.guard';
 
 const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'auction', component: AuctionListComponent },
+  {path: '', component: HomeComponent},
+  {path: 'users/:id', component: ProfileComponent},
+  {path: 'user/edit/:id', component: RegisterComponent},
+  { path: '', component: HomeComponent },
+  { path: 'auction/:id', component: AuctionDetailComponent },
+  { path: 'category/:id', component: AuctionListComponent, pathMatch: 'full' },
+  { path: 'auction/:id', component: AuctionDetailComponent, canActivate: [AuthenticationGuard]},
+   { path: '', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
