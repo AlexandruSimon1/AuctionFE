012ee939e08d9176b0f8b64beac7ade2449ed3df
Arthur_Simon <asimon@ms3-inc.com>
2022-02-18 11:28:48 +0200

    Removed no needed function

--- src/app/security/authentication.service.ts
+++ src/app/security/authentication.service.ts
@@ -4,9 +4,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
-import { state } from '@angular/animations';
 import { Router } from '@angular/router';
-
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
@@ -14,25 +12,21 @@ export class AuthenticationService {
     public currentUser: Observable<User>;
     private baseUrl = `${environment.baseAPIUrl}/${environment.api.authorization}`;
 
-
     constructor(private http: HttpClient,private router: Router) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
         var CronJob = require('cron').CronJob;
         var job = new CronJob('* 59 * * * *', function () {
-            console.log("Removing Current User Logged In System")
             localStorage.removeItem('currentUser');
             this.currentUserSubject.next(null);
             this.router.navigate(['/login']);
-        }, null, true, 'Europe/Zurich');
+        }, null, true, 'Europe/Bucharest');
         job.start();
     }
 
     public get currentUserValue(): User {
         return this.currentUserSubject.value;
     }
-
-
 
     login(username: string, password: string) {
         return this.http.post<any>(`${this.baseUrl}/signin`, { username, password })
